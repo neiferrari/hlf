3426dc4e20a4c4f8e49ca687dd6e359632822ecc
NeiFerrari <nei.ferrari@gmail.com>
2023-06-27 19:34:26 +0200

    Remove Notebook File

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.player import Player
+from src.board import Board
+
+player_player = Player()
+player_pc = Player()
+board_player = Board()
+board_pc = Board()
+
+print(player_player)
+print(board_pc)
